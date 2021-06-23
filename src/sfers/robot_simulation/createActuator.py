dc50ab0effc2697704e0871e80d01552b84551e8
zhiwuz <zhwzheng.1994@gmail.com>
2021-06-23 19:42:58 -0400

    Remove semicolons; Update material parameters

--- src/sfers/robot_simulation/createActuator.py
+++ src/sfers/robot_simulation/createActuator.py
@@ -1,11 +1,3 @@
-#Created on 08/04/2019 by Zhiwu Zheng at Princeton University
-#Revision record:
-#08/05/2019: add flag segment at the end
-#08/06/2019: add generate1DMotorVoltages function
-#08/07/2019: return jointNumber
-#09/02/2019: use list comprehension for generate1DMotorVoltages function
-#09/10/2019: add voltageTorqueControlStep function
-
 import pybullet as p
 
 def create1DMultiActuators(actuatorNumber,unitMotorNumber,
@@ -13,60 +5,58 @@ def create1DMultiActuators(actuatorNumber,unitMotorNumber,
                            actuatorWidth,actuatorThickness,
                            basePosition=[0,0,0],baseOrientation=[0,0,0,1]):
 
-    N=actuatorNumber;
-    m=unitMotorNumber;
-    thickness=actuatorThickness;
-    width=actuatorWidth;
-    linkLength=actuatorLength/m;
-    jointLength=0.5*linkLength;
-    flagLength=0.0;
-    flagMass=0.0;
-    
+    N=actuatorNumber
+    m=unitMotorNumber
+    thickness=actuatorThickness
+    width=actuatorWidth
+    linkLength=actuatorLength/m
+    jointLength=0.5*linkLength
+    flagLength=0.0
+    flagMass=0.0
+
     startBoxId = p.createCollisionShape(p.GEOM_BOX,
-                                        halfExtents=[0.5*jointLength, 0.5*width, 0.5*thickness]);
+                                        halfExtents=[0.5*jointLength, 0.5*width, 0.5*thickness])
     linkBoxId = p.createCollisionShape(p.GEOM_BOX,
                                        halfExtents=[0.5*linkLength, 0.5*width, 0.5*thickness],
-                                       collisionFramePosition=[0.5*linkLength,0,0]);
+                                       collisionFramePosition=[0.5*linkLength,0,0])
     endBoxId = p.createCollisionShape(p.GEOM_BOX,
                                       halfExtents=[0.5*jointLength, 0.5*width, 0.5*thickness],
-                                      collisionFramePosition=[0.5*jointLength,0,0]);
+                                      collisionFramePosition=[0.5*jointLength,0,0])
 
     flagBoxId = p.createCollisionShape(p.GEOM_BOX,
                                       halfExtents=[0.5*flagLength, 0.5*width, 0.5*thickness],
-                                      collisionFramePosition=[0.5*flagLength,0,0]);
+                                      collisionFramePosition=[0.5*flagLength,0,0])
 
-        
-    mass = actuatorMass/(2*m);
-    visualShapeId = -1;
-    basePosition=basePosition;
-    baseOrientation = baseOrientation;
+    mass = actuatorMass/(2*m)
+    visualShapeId = -1
+    basePosition=basePosition
+    baseOrientation = baseOrientation
 
     link_Masses = [actuatorMass/m for i in range(N*m-1)]
-    link_Masses.append(actuatorMass/(2*m));
-    link_Masses.append(flagMass);
+    link_Masses.append(actuatorMass/(2*m))
+    link_Masses.append(flagMass)
 
     linkCollisionShapeIndices = [linkBoxId for i in range(N*m-1)]
-    linkCollisionShapeIndices.append(endBoxId);
-    linkCollisionShapeIndices.append(flagBoxId);
+    linkCollisionShapeIndices.append(endBoxId)
+    linkCollisionShapeIndices.append(flagBoxId)
 
-    linkVisualShapeIndices = [-1 for i in range(N*m+1)];
+    linkVisualShapeIndices = [-1 for i in range(N*m+1)]
 
-    linkPositions=[[0.5*jointLength, 0, 0]];
+    linkPositions=[[0.5*jointLength, 0, 0]]
     for i in range(N*m-1):
-        linkPositions.append([linkLength, 0, 0]);
-    linkPositions.append([jointLength, 0, 0]);
-
+        linkPositions.append([linkLength, 0, 0])
+    linkPositions.append([jointLength, 0, 0])
 
     linkOrientations = [[0, 0, 0, 1] for i in range(N*m+1)]
 
     linkInertialFramePositions = [[0.5*linkLength, 0, 0] for i in range(N*m-1)]
-    linkInertialFramePositions.append([0.5*jointLength, 0, 0]);
-    linkInertialFramePositions.append([0.5*flagLength, 0, 0]);
+    linkInertialFramePositions.append([0.5*jointLength, 0, 0])
+    linkInertialFramePositions.append([0.5*flagLength, 0, 0])
 
     linkInertialFrameOrientations = [[0, 0, 0, 1] for i in range(N*m+1)]
     indices = [i for i in range(N*m+1)]
     jointTypes = [p.JOINT_REVOLUTE for i in range(N*m)]
-    jointTypes.append(p.JOINT_FIXED);
+    jointTypes.append(p.JOINT_FIXED)
     axis = [[0, 1, 0] for i in range(N*m+1)]
 
     boxId = p.createMultiBody(mass,
@@ -83,46 +73,42 @@ def create1DMultiActuators(actuatorNumber,unitMotorNumber,
                               linkInertialFrameOrientations=linkInertialFrameOrientations,
                               linkParentIndices=indices,
                               linkJointTypes=jointTypes,
-                              linkJointAxis=axis);
+                              linkJointAxis=axis)
 
-    jointNumber=p.getNumJoints(boxId)-1;
+    jointNumber=p.getNumJoints(boxId)-1
     #Disable the default motors
     for joint in range(jointNumber):
         p.setJointMotorControl2(boxId,
                                 joint,
                                 p.VELOCITY_CONTROL,
                                 force=0)
-    return [boxId, jointNumber];
+    return [boxId, jointNumber]
 
 
 def generate1DMotorVoltages(actuatorVoltages,actuatorNumber,unitMotorNumber):
-    N=actuatorNumber;
-    m=unitMotorNumber;
+    N=actuatorNumber
+    m=unitMotorNumber
     motorVoltages=[actuatorVoltages[i]/m for i in range(N) for j in range(m)]
-    return motorVoltages;
-
+    return motorVoltages
 
 
 def voltageTorqueControlStep(boxId,actuatorVoltages,TorVolThe, N, m, jointNumber,jointIndex,linkIndex):
-    import pybullet as p
-    from createActuator import generate1DMotorVoltages
-    
-    motorVoltages=generate1DMotorVoltages(actuatorVoltages, N, m);
-    theta=[];
-    angularVelocities=[];
-    positions=[];
-    jointStates=p.getJointStates(boxId,jointIndex);
-    linkStates=p.getLinkStates(boxId,linkIndex);
-    positions.append(p.getBasePositionAndOrientation(boxId)[0]); #append base position
+    motorVoltages=generate1DMotorVoltages(actuatorVoltages, N, m)
+    theta=[]
+    angularVelocities=[]
+    positions=[]
+    jointStates=p.getJointStates(boxId,jointIndex)
+    linkStates=p.getLinkStates(boxId,linkIndex)
+    positions.append(p.getBasePositionAndOrientation(boxId)[0])  #append base position
     for joint in range(jointNumber):
-        theta.append(jointStates[joint][0]);
-        angularVelocities.append(jointStates[joint][1]);
-        positions.append(linkStates[joint][4]);
+        theta.append(jointStates[joint][0])
+        angularVelocities.append(jointStates[joint][1])
+        positions.append(linkStates[joint][4])
 
-    positions.append(linkStates[jointNumber][4]); #append the flag position
+    positions.append(linkStates[jointNumber][4])  #append the flag position
     Tor=[TorVolThe(theta[joint],angularVelocities[joint],motorVoltages[joint]) for joint in range(jointNumber)]
     p.setJointMotorControlArray(boxId,
                            jointIndex,
                            p.TORQUE_CONTROL,
                            forces=Tor)
-    return [theta,angularVelocities,positions,motorVoltages,Tor];
+    return [theta,angularVelocities,positions,motorVoltages,Tor]
--- src/sfers/robot_simulation/inchwormEnvs.py
+++ src/sfers/robot_simulation/inchwormEnvs.py
@@ -1,139 +1,127 @@
-import os
-import random
 import pybullet as p
 import pybullet_data
-import time
 import math
 import numpy as np
-import datetime
-from sfers.robot_simulation.createActuator import create1DMultiActuators,generate1DMotorVoltages,voltageTorqueControlStep
+from sfers.robot_simulation.createActuator import create1DMultiActuators, generate1DMotorVoltages, \
+    voltageTorqueControlStep
 
 
-class inchworm:
+class inchworm(object):
     def __init__(self):
-        self.frictionLow = 2.5
+        self.frictionLow = 1.0
         self.frictionHigh = 2.5
         self.groundFriction = 1.0
         self.isGravity = True
-        self.subThickness=0.00508
-        self.actThickness=0.03
-        self.actuatorLength=10;
-        self.width=6;
-        
-        self.halfEI = 10.0456475 * 1e5
-
-        self.drivenFrequency=1; #in Hz
-
-        self.N=5;
-        self.m=1;
-        self.thickness=0.1;
-        self.simTime=0;
-        self.step=0
-        
-        self.beta = 1.46e-4  #Condition 9(j) - beta_1
-        
-        self.timeStep=0.01/240;
-        self.dampingEta=0.9;
-        self.recordStepInterval=125
+        self.subThickness = 0.00508
+        self.actThickness = 0.03
+        self.actuatorLength = 10
+        self.width = 2
+        self.halfEI = 1.2595446893110755e6
+        self.drivenFrequency = 1  # in Hz
+        self.N = 5
+        self.m = 1
+        self.thickness = 0.1
+        self.simTime = 0
+        self.step = 0
+        self.beta = 3.6327811654911875e-4
+        self.timeStep = 0.01 / 240
+        self.dampingEta = 0.2
+        self.recordStepInterval = 125
         self.simCycles = 30000
+        self.dataTime = []
+        self.dataTheta = []
+        self.dataAngularVelocities = []
+        self.dataTor = []
+        self.dataPositions = []
+        self.dataMotorVoltages = []
+        self.theta = []
+        self.angularVelocities = []
+        self.positions = []
 
-
-        
-        self.dataTime=[];
-        self.dataTheta=[];
-        self.dataAngularVelocities=[];
-        self.dataTor=[];
-        self.dataPositions=[];
-        self.dataMotorVoltages=[];
-        self.theta=[];
-        self.angularVelocities=[];
-        self.positions=[];
-        
     def simReset(self):
-        self.actuator2DDensity=8*self.subThickness+5.44*self.actThickness;
-        self.actuator1DDensity=self.actuator2DDensity*self.width;
-        self.actuatorMass=self.actuator1DDensity*self.actuatorLength;
-        self.lateralFrictionCoefficients=[self.frictionLow for _ in range((self.N-1)*self.m+int(math.ceil(2*self.m/3)))]
-        for _ in range(self.m+1-int(math.ceil(2*self.m/3))):
+        self.actuator2DDensity = 8.1 * self.subThickness + 6.6 * self.actThickness
+        self.actuator1DDensity = self.actuator2DDensity * self.width
+        self.actuatorMass = self.actuator1DDensity * self.actuatorLength
+        self.lateralFrictionCoefficients = [self.frictionLow for _ in
+                                            range((self.N - 1) * self.m + int(math.ceil(2 * self.m / 3)))]
+        for _ in range(self.m + 1 - int(math.ceil(2 * self.m / 3))):
             self.lateralFrictionCoefficients.append(self.frictionHigh)
-        self.linkLength=self.actuatorLength/self.m;
-        self.jointLength=0.5*self.linkLength;
-                
+        self.linkLength = self.actuatorLength / self.m
+        self.jointLength = 0.5 * self.linkLength
+
         p.connect(p.DIRECT)
         p.resetSimulation()
-        p.resetDebugVisualizerCamera(self.N*15, -360, -16, [0, 0, 1])
+        p.resetDebugVisualizerCamera(self.N * 15, -360, -16, [0, 0, 1])
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        self.planeId=p.loadURDF('plane.urdf',basePosition=[0,0,-0.5*self.thickness], globalScaling=10.0);
-        (self.boxId,self.jointNumber)=create1DMultiActuators(self.N,self.m,self.actuatorMass,
-                                                             self.actuatorLength,self.width,self.thickness);
+        self.planeId = p.loadURDF('plane.urdf', basePosition=[0, 0, -0.5 * self.thickness], globalScaling=10.0)
+        (self.boxId, self.jointNumber) = create1DMultiActuators(self.N, self.m, self.actuatorMass,
+                                                                self.actuatorLength, self.width, self.thickness)
 
-        self.jointIndex=range(self.jointNumber);
-        self.linkIndex=range(self.jointNumber+1);
-        p.setTimeStep(self.timeStep);
+        self.jointIndex = range(self.jointNumber)
+        self.linkIndex = range(self.jointNumber + 1)
+        p.setTimeStep(self.timeStep)
         if self.isGravity:
             p.setGravity(0, 0, -980)
         else:
             p.setGravity(0, 0, -1)
-        
-        p.changeDynamics(self.planeId, -1, spinningFriction=0.0, rollingFriction=0.0, lateralFriction=self.groundFriction,
-                         linearDamping=0.0, angularDamping=0.0,jointDamping=0.0)
-        p.changeDynamics(self.planeId, 0, spinningFriction=0.0, rollingFriction=0.0, lateralFriction=self.groundFriction,
-                         linearDamping=0.0, angularDamping=0.0,jointDamping=0.0)
+
+        p.changeDynamics(self.planeId, -1, spinningFriction=0.0, rollingFriction=0.0,
+                         lateralFriction=self.groundFriction,
+                         linearDamping=0.0, angularDamping=0.0, jointDamping=0.0)
+        p.changeDynamics(self.planeId, 0, spinningFriction=0.0, rollingFriction=0.0,
+                         lateralFriction=self.groundFriction,
+                         linearDamping=0.0, angularDamping=0.0, jointDamping=0.0)
         p.changeDynamics(self.boxId, -1, spinningFriction=0.0, rollingFriction=0.0,
                          lateralFriction=self.lateralFrictionCoefficients[0],
-                         linearDamping=0.0, angularDamping=0.0,jointDamping=0.0)
+                         linearDamping=0.0, angularDamping=0.0, jointDamping=0.0)
         for joint in self.jointIndex:
-            p.changeDynamics(self.boxId,joint,spinningFriction=0.0, rollingFriction=0.0,
-                             lateralFriction=self.lateralFrictionCoefficients[joint+1],
-                             linearDamping=0.0, angularDamping=0.0,jointDamping=0.0)
-            
+            p.changeDynamics(self.boxId, joint, spinningFriction=0.0, rollingFriction=0.0,
+                             lateralFriction=self.lateralFrictionCoefficients[joint + 1],
+                             linearDamping=0.0, angularDamping=0.0, jointDamping=0.0)
+
     def reset(self):
         self.simReset()
-    
-    def TorVolThe(self,Theta,angularVelocity,Voltage):
-        #thetaTarget=-1.5e-3*Voltage;
-        #halfEI=3.973*1e5;
-        #thetaTarget=-1.6e-4*Voltage;
-        thetaTarget=-self.beta*Voltage; 
-        #thetaTarget=-1.4587e-3*Voltage;
-        K=self.halfEI/self.jointLength;
-        omega=2*math.pi*self.drivenFrequency;
-        Tor=-self.width*K*((Theta-thetaTarget)+(self.dampingEta/omega)*angularVelocity);
+
+    def TorVolThe(self, Theta, angularVelocity, Voltage):
+        thetaTarget = -self.beta * Voltage
+        K = self.halfEI / self.jointLength
+        omega = 2 * math.pi * self.drivenFrequency
+        Tor = -self.width * K * ((Theta - thetaTarget) + (self.dampingEta / omega) * angularVelocity)
         return Tor
-    
+
     def simStep(self, actuatorVoltages):
-        if (self.step+1)%100000==0:
-            print("step=",self.step+1);
-            
-        
-        self.simTime=self.step*self.timeStep
-        [self.theta,self.angularVelocities,self.positions,self.motorVoltages,self.Tor]=voltageTorqueControlStep(self.boxId,
-                                                                                       actuatorVoltages,
-                                                                                       self.TorVolThe, 
-                                                                                       self.N, 
-                                                                                       self.m, 
-                                                                                       self.jointNumber,
-                                                                                       self.jointIndex,
-                                                                                       self.linkIndex);
+        if (self.step + 1) % 100000 == 0:
+            print("step=", self.step + 1)
 
-        if (self.step+1)%self.recordStepInterval==0 or self.step==0:
-            self.dataTime.append(self.simTime);
-            self.dataTheta.append(self.theta);
-            self.dataAngularVelocities.append(self.angularVelocities);
-            self.dataPositions.append(self.positions);
-            self.dataMotorVoltages.append(self.motorVoltages);
-            self.dataTor.append(self.Tor);
+        self.simTime = self.step * self.timeStep
+        [self.theta, self.angularVelocities, self.positions, self.motorVoltages, self.Tor] = voltageTorqueControlStep(
+            self.boxId,
+            actuatorVoltages,
+            self.TorVolThe,
+            self.N,
+            self.m,
+            self.jointNumber,
+            self.jointIndex,
+            self.linkIndex)
 
-        p.stepSimulation();
-        
+        if (self.step + 1) % self.recordStepInterval == 0 or self.step == 0:
+            self.dataTime.append(self.simTime)
+            self.dataTheta.append(self.theta)
+            self.dataAngularVelocities.append(self.angularVelocities)
+            self.dataPositions.append(self.positions)
+            self.dataMotorVoltages.append(self.motorVoltages)
+            self.dataTor.append(self.Tor)
+
+        p.stepSimulation()
+
         self.step += 1
-        
+
     def controlStep(self, actuatorVoltages):
         for simStep in range(self.simCycles):
             self.simStep(actuatorVoltages)
-            
-        return [list(-1*np.array(self.theta)),list(-1*np.array(self.angularVelocities)),self.positions,
-                self.motorVoltages,self.Tor]
-    def close(self):
-        p.disconnect();
 
+        return [list(-1 * np.array(self.theta)), list(-1 * np.array(self.angularVelocities)), self.positions,
+                self.motorVoltages, self.Tor]
+
+    def close(self):
+        p.disconnect()
